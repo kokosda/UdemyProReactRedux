59fd8f90ab9076fd5e3934f4847d2e8c688ad406
Victor Ponamarev <vponamrev@gmail.com>
2019-12-17 08:38:47 +0300

    Routing - Link and details page.

--- star-db/src/components/app/app.js
+++ star-db/src/components/app/app.js
@@ -12,6 +12,7 @@ import { PeoplePage, PlanetsPage, StarshipsPage } from "../pages";
 import './app.css';
 
 import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { StarshipDetails } from '../sw-components';
 
 export default class App extends Component {
 
@@ -41,13 +42,16 @@ export default class App extends Component {
 
               <RandomPlanet />
 
+              <Route path="/" render={() => <h2>Welcome to StarDB</h2>} exact />
               <Route path="/people" component={PeoplePage} />
               <Route path="/planets" component={PlanetsPage} />
-              <Route path="/starships" component={StarshipsPage} />
+              <Route path="/starships" component={StarshipsPage} exact />
+              <Route path="/starships/:id" 
+                      render={({match, location, history}) => {
+                        const {id} = match.params;
+                        return <StarshipDetails itemId={id} />;
+                      }} />
 
-              {/* <PeoplePage/>
-              <PlanetsPage />
-              <StarshipsPage /> */}
             </div>
           </Router>
         </SwapiServiceProvider>
