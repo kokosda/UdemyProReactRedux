8c72d6dc93a64d32921884ea75568a5e10fd0ec2
Victor Ponamarev <vponamrev@gmail.cm>
2019-12-19 09:30:09 +0300

    Route - switch component helps to show page not found (redirect or route can be used).

--- star-db/src/components/app/app.js
+++ star-db/src/components/app/app.js
@@ -11,7 +11,7 @@ import { PeoplePage, PlanetsPage, StarshipsPage, LoginPage, SecretPage } from ".
 
 import './app.css';
 
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import { StarshipDetails } from '../sw-components';
 
 export default class App extends Component {
@@ -50,23 +50,27 @@ export default class App extends Component {
 
               <RandomPlanet />
 
-              <Route path="/" render={() => <h2>Welcome to StarDB</h2>} exact />
-              <Route path="/people/:id?" component={PeoplePage} />
-              <Route path="/planets" component={PlanetsPage} />
-              <Route path="/starships" component={StarshipsPage} exact />
-              <Route path="/starships/:id" 
-                      render={({match, location, history}) => {
-                        const {id} = match.params;
-                        return <StarshipDetails itemId={id} />;
-                      }} />
-              <Route path="/login" 
-                      render={() => {
-                        return <LoginPage isLoggedIn={isLoggedIn} onLogin={this.onLogin} />;
-                      }} />
-              <Route path="/secret"
-                      render={() => {
-                        return <SecretPage isLoggedIn={isLoggedIn} />;
-                      }} />
+              <Switch>
+                <Route path="/" render={() => <h2>Welcome to StarDB</h2>} exact />
+                <Route path="/people/:id?" component={PeoplePage} />
+                <Route path="/planets" component={PlanetsPage} />
+                <Route path="/starships" component={StarshipsPage} exact />
+                <Route path="/starships/:id" 
+                        render={({match, location, history}) => {
+                          const {id} = match.params;
+                          return <StarshipDetails itemId={id} />;
+                        }} />
+                <Route path="/login" 
+                        render={() => {
+                          return <LoginPage isLoggedIn={isLoggedIn} onLogin={this.onLogin} />;
+                        }} />
+                <Route path="/secret"
+                        render={() => {
+                          return <SecretPage isLoggedIn={isLoggedIn} />;
+                        }} />
+
+                <Route render={() => <h2>Page not found</h2> } />
+              </Switch>
 
             </div>
           </Router>
