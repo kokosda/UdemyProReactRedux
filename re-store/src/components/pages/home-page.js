99552220ac0cb2f1aefe260522307023cdfe2ee0
Victor Ponamarev <vponamrev@gmail.cm>
2019-12-22 16:44:24 +0300

    react+redux - routing, book-list, book-list-item.

---
+++ re-store/src/components/pages/home-page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import BooksList from '../book-list';
+import BookList from '../book-list';
+
+const HomePage = () => {
+    return (
+        <BookList books={[]} />
+    );
+};
+
+export default HomePage;
