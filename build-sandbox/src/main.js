304a506bde687d617b0618d81fc620638dc64f83
Victor Ponamarev <vponamrev@gmail.com>
2019-12-29 01:56:11 +0300

    babel - babel uses core-js polyfills, recommended approach is useBuiltIns: usage that substitues only required functions not all of them.

--- build-sandbox/src/main.js
+++ build-sandbox/src/main.js
@@ -1,8 +1,11 @@
 class App {
-    run = (name = 'World') => {
+    run = async (name = 'World') => {
         console.log(`Hello ${name}`);
+        console.log([1, 2, [2, 3]].flat());
     }
 }
 
-app = new App();
-app.run();
+const app = new App();
+app.run()
+    .then(() => console.log('done'))
+    .cathc(() => console.log('Error!'));
