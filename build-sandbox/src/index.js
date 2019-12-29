89e5ff79f93bbd4cc8f80a8ead900fd893ddf3ae
Victor Ponamarev <vponamrev@gmail.com>
2019-12-29 14:21:52 +0300

    webpack - loaders' composition - loaders are handled from last to the most recent, so one file can be handled by multiple loaders at a time.

--- build-sandbox/src/index.js
+++ build-sandbox/src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import css from './main.css';
+
+console.log(css.toString());
 
 const App = () => <p>Hello</p>;
 
--- build-sandbox/webpack.config.js
+++ build-sandbox/webpack.config.js
@@ -30,6 +30,14 @@ module.exports = {
                         name: "[name].[ext]"
                     }
                 }]
+            },
+
+            {
+                test: /\.(css)$/,
+                use: [
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader' }
+                ]
             }
         ]
     }
